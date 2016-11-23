8f2e7102ea982da79b181cfda973bb8e22d28145
王亮 <wangliang@zhongzhihui.com>
2016-11-23 09:05:33 +0800

    N/A

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,13 @@
 require.config({
+<<<<<<< 436a51d8af33e50f50616a05d9e536d612e40d34
     baseUrl: '//192.168.0.102:3000/m-service-market/dist/js/',
     paths: {
         'lib': '//192.168.0.102:3000/lib',
+=======
+    baseUrl: '//192.168.1.76:3000/m-service-market/dist/js/',
+    paths: {
+        'lib': '//192.168.1.76:3000/lib',
+>>>>>>> N/A
         'css': 'lib/require/2.1/plugins/css/css', // or whatever the path to require-css is
         'text': 'lib/require/2.1/plugins/text/text', // or whatever the path to require-css is
         'jquery': 'lib/zeptojs/1.1.5/zepto'
--- source/js/conf/publish-require.js
+++ source/js/conf/publish-require.js
@@ -5,7 +5,117 @@ define(function(require, exports, module) {
     'use strict';
     var $ = require('jquery');
     var box = require('lib/ui/box/1.0.1/box');
-    $(".mod-classify .list").on('click', function() {
-        $(".mod-classify .title input").text($(this).text());
+
+    //分类
+    $('#jClassify').on('tap','label',function(e) {
+        if($(e.target).hasClass('checked')){
+            $(e.target).removeClass('checked');
+        }else {
+            $(e.target).addClass('checked');
+        }
+    });
+
+    //报价
+    var config = {
+        "value0":"<input type='text' placeholder='0元'>"
+                +"<span>至</span>"
+                +"<input type='text' placeholder='0元'>",
+        "value1":"<input type='text' placeholder='0元'>",
+        "value2":"<b style='font-weight: normal;'>价格以面议为准</b>"
+    };
+
+    $('.jLabel').each(function(){
+        if($(this).next('input[type="radio"]').val() === '0'){
+            $(this).addClass('current');
+            $(this).next('input[type="radio"]').prop('checked',true);
+        }
+    });
+
+    $('.jLabel').on('tap',function(){
+        $(this).addClass('current').siblings().removeClass('current');
+        var val = $(this).next('input[type="radio"]').val();
+        switch (val)
+        {
+            case '0':
+                $('.jPrice').html(config.value0);
+                break;
+            case '1':
+                $('.jPrice').html(config.value1);
+                break;
+            case '2':
+                $('.jPrice').html(config.value2);
+                break;
+        };
+    });
+
+    //需求截止时间
+    var date = new Date().getFullYear()+"年"+(new Date().getMonth()+1)+"月"+new Date().getDate()+"日";
+    $('.date .day').text(date);
+    $('#jDate').on('change',function(){
+        date = $(this).val().replace("-","年").replace("-","月")+"日";
+        $('.date .day').text(date);
+    });
+
+    //验证必填项
+    function validation(){
+        var message = '';
+        $('.jRequired').children('input[type="text"]').each(function(){
+            if($(this).val() === ''){
+                message = '请输入标题、联系人和联系方式';
+            }
+        });
+
+        var checkboxL = $('.jRequired').children('input[type="checkbox"]:checked').length;
+        if(checkboxL === 0){
+            message = '请选择分类';
+        }
+
+        var radio = $('.jRequired').children('input[type="radio"]:checked');
+        var radioL = radio.length;
+        if(radioL === 0){
+            message = '请选择报价方式';
+        }else if(radio.val() !== "2"){
+            $('.jRequired').children('.price').children('input[type="text"]').each(function(){
+                if($(this).val() === ''){
+                    message = '请输入预算或一口价';
+                }
+            });
+        }
+
+        $('.jRequired').children('textarea').each(function(){
+            if($(this).val() === ''){
+                message = '请输入需求描述';
+            }
+        })
+
+        return message;
+    };
+
+    $('.jRequired').find('input[type="checkbox"],input[type="radio"]').on('change',function(){
+        if(validation() === ''){
+            $('input[type="submit"]').addClass('submit');
+        }else {
+            $('input[type="submit"]').removeClass('submit');
+        }
+    });
+
+    $('.jRequired').find('input[type="text"]').on('blur',function(){
+        if(validation() == ''){
+            $('input[type="submit"]').addClass('submit');
+        }else {
+            $('input[type="submit"]').removeClass('submit');
+        }
+    });
+
+    validation();
+    $('#jForm').on('submit',function(){
+        var isSubmit = true;
+        var message = validation();
+        console.log(message);
+        if(message != ''){
+            isSubmit = false;
+            box.warn(message);
+        }
+        return isSubmit;
     });
 });
