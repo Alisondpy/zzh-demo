f7e8cddddf4571fd666c68074b066ac2091f69c1
jiangchaoyi <jiangchaoyi@bubugao.com>
2016-11-24 13:51:55 +0800

    N/A

--- source/js/plugins/pull-to-refresh/1.0.0/pull-to-refresh.js
+++ source/js/plugins/pull-to-refresh/1.0.0/pull-to-refresh.js
@@ -33,13 +33,13 @@ define(function(require, exports, module) {
             iscrollOptions: {
                 probeType: 3, //probeType：1对性能没有影响。在滚动事件被触发时，滚动轴是不是忙着做它的东西。probeType：2总执行滚动，除了势头，反弹过程中的事件。这类似于原生的onscroll事件。probeType：3发出的滚动事件与到的像素精度。注意，滚动被迫requestAnimationFrame（即：useTransition：假）。  
                 scrollbars: true, //有滚动条  
-                // mouseWheel: true, //允许滑轮滚动  
-                // fadeScrollbars: true, //滚动时显示滚动条，默认影藏，并且是淡出淡入效果  
-                // bounce: true, //边界反弹  
-                // interactiveScrollbars: true, //滚动条可以拖动  
-                // shrinkScrollbars: 'scale', // 当滚动边界之外的滚动条是由少量的收缩。'clip' or 'scale'.  
-                // click: true, // 允许点击事件  
-                // momentum: true // 允许有惯性滑动 
+                mouseWheel: true, //允许滑轮滚动  
+                fadeScrollbars: true, //滚动时显示滚动条，默认影藏，并且是淡出淡入效果  
+                bounce: true, //边界反弹  
+                interactiveScrollbars: true, //滚动条可以拖动  
+                shrinkScrollbars: 'scale', // 当滚动边界之外的滚动条是由少量的收缩。'clip' or 'scale'.  
+                click: true, // 允许点击事件  
+                momentum: true // 允许有惯性滑动 
             }
         };
 
@@ -133,6 +133,7 @@ define(function(require, exports, module) {
                     }
                 }
             } else {
+                _this._isLoading = false;
                 _this.pullDown.hide();
                 _this.pullUp.hide();
                 _this.iscroll.refresh();
