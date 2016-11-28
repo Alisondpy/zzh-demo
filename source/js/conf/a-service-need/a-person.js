612231e694a261e1a0afb2e20af4b78acf125369
zhanglihong <zhanglihong@zhongzhihui.com>
2016-11-28 16:11:54 +0800

    AAA

--- source/js/conf/a-service-need/a-person.js
+++ source/js/conf/a-service-need/a-person.js
@@ -5,10 +5,21 @@ define(function(require, exports, module) {
     var io = require('lib/core/1.0.0/io/request');
     var form = require('lib/core/1.0.0/utils/form');
 
-    console.log('接受成功');
+    //console.log('接受成功');
+    if($('.jNum').text()>0) {
+        $('.jNum').css({'display':'block'});
+    }else{
+        $('.jNum').css({'display':'none'});
+    };
     $('.jNeed').on('tap', function () {
         window.location.href = '/m-service-market/dist/html/a-service-need.html?status=0';
-    })
+    });
+    $('.jGoing').on('tap', function () {
+        window.location.href = '/m-service-market/dist/html/a-service-need.html?status=1';
+    });
+    $('.jOver').on('tap', function () {
+        window.location.href = '/m-service-market/dist/html/a-service-need.html?status=2';
+    });
     /*io.get('url', {}, function(){
     	
     })*/
