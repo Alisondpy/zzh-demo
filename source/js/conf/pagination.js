7899bfb73c92bd24567d4cab5b34f04a7321a2fe
jiangchaoyi <jiangchaoyi@bubugao.com>
2016-12-02 13:52:54 +0800

    N/A

--- source/js/conf/pagination.js
+++ source/js/conf/pagination.js
@@ -9,35 +9,35 @@ define(function(require, exports, module) {
     var $ = require('jquery');
     require('lib/ui/pagination/2.0.7/pagination');
 
-    // var IO = require('lib/core/1.0.0/io/request');
+    var IO = require('lib/core/1.0.0/io/request');
 
-    // var dataContainer = $('#jDataContainer');
+    var dataContainer = $('#jDataContainer');
 
-    // $('#jPaginationContainer').pagination({
-    //     dataSource: $PAGE_DATA['staticBaseUrl'] + '/source/api/pager.json',
-    //     locator: 'items',
-    //     totalNumber: 120,
-    //     pageSize: 20,
-    //     ajax: {
-    //         beforeSend: function() {
-    //             dataContainer.html('Loading data from flickr.com ...');
-    //         }
-    //     },
-    //     callback: function(data, pagination) {
-    //         // template method of yourself
-    //         var html = simpleTemplating(data);
-    //         dataContainer.html(html);
-    //     }
-    // })
+    $('#jPaginationContainer').pagination({
+        dataSource: $PAGE_DATA['staticBaseUrl'] + '/source/api/pager.json',
+        locator: 'items',
+        totalNumber: 120,
+        pageSize: 20,
+        ajax: {
+            beforeSend: function() {
+                dataContainer.html('Loading data from flickr.com ...');
+            }
+        },
+        callback: function(data, pagination) {
+            // template method of yourself
+            var html = simpleTemplating(data);
+            dataContainer.html(html);
+        }
+    })
 
-    // function simpleTemplating(data) {
-    //     var html = '<ul>';
-    //     $.each(data, function(index, item) {
-    //         html += '<li>' + item + '</li>';
-    //     });
-    //     html += '</ul>';
-    //     return html;
-    // }
+    function simpleTemplating(data) {
+        var html = '<ul>';
+        $.each(data, function(index, item) {
+            html += '<li>' + item + '</li>';
+        });
+        html += '</ul>';
+        return html;
+    }
 
 
 });
