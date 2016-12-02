2a27719ca9a45da9efcdf49e647903febfc8d4f7
jiangchaoyi <jiangchaoyi@bubugao.com>
2016-12-02 13:51:16 +0800

    N/A

--- source/js/conf/pagination.js
+++ source/js/conf/pagination.js
@@ -7,69 +7,37 @@ define(function(require, exports, module) {
     'use strict';
 
     var $ = require('jquery');
-    var Pagination = require('lib/ui/pagination/1.0.1/pagination');
+    require('lib/ui/pagination/2.0.7/pagination');
 
-    var IO = require('lib/core/1.0.0/io/request');
+    // var IO = require('lib/core/1.0.0/io/request');
 
-    var pager = $('#jPagination');
+    // var dataContainer = $('#jDataContainer');
 
-    var random = function(min, max) {
-        return ~~(Math.random() * (max - min + 1)) + min;
-    };
+    // $('#jPaginationContainer').pagination({
+    //     dataSource: $PAGE_DATA['staticBaseUrl'] + '/source/api/pager.json',
+    //     locator: 'items',
+    //     totalNumber: 120,
+    //     pageSize: 20,
+    //     ajax: {
+    //         beforeSend: function() {
+    //             dataContainer.html('Loading data from flickr.com ...');
+    //         }
+    //     },
+    //     callback: function(data, pagination) {
+    //         // template method of yourself
+    //         var html = simpleTemplating(data);
+    //         dataContainer.html(html);
+    //     }
+    // })
 
-    var pagination = new Pagination(pager, {
-        pageSize: 10,
-        totalCount: 100,
-        displayInfo: true,
-        onPage:function(pageNum, e){
-            console.log('pageNum',pageNum);
-        }
+    // function simpleTemplating(data) {
+    //     var html = '<ul>';
+    //     $.each(data, function(index, item) {
+    //         html += '<li>' + item + '</li>';
+    //     });
+    //     html += '</ul>';
+    //     return html;
+    // }
 
-        // Implements a async paging api method.
-        // async: function(pageOpts) {
-        //     console.log('async paging options => ', pageOpts);
-
-
-        //     var deferred = $.Deferred();
-
-        //     IO.get($PAGE_DATA['baseStaticUrl'] + '/source/api/pager.json', function(data) {
-        //         console.log(data);
-        //         deferred.resolve(data)
-        //         // console.log(data);
-        //     });
-
-        //     return deferred.promise();
-
-        //     // // simulating async paging
-        //     // setTimeout(function() {
-        //     //     deferred.resolve({ totalCount: random(100, 500) });
-        //     // }, 100);
-        //     // 
-        //     // 
-
-        //     // io.get('',{},function(){
-
-        //     // });
-
-        //     // return deferred.promise();
-        // }
-    });
-
-    $('.selectPage').click(function() { pagination.selectPage(random(10, 100)); });
-    $('.setPageSize').click(function() { pagination.setPageSize(20); });
-    $('.setTotalCount').click(function() { pagination.setTotalCount(random(500, 1000)); });
-
-    $('.disable').click(function() { pagination.disable(); });
-    $('.enable').click(function() { pagination.enable(); });
-    $('.destroy').click(function() { pagination.destroy(); });
-
-    $('.getPageData').click(function() {
-        var data = [
-            'totalCount: ' + pagination.get('totalCount'),
-            'pageCount: ' + pagination.get('pageCount'),
-            'currentPage: ' + pagination.get('currentPage')
-        ];
-        console.log(data.join(', '));
-    });
 
 });
