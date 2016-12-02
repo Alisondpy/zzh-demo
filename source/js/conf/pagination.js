53c7e89986ed82a6c988a7916131332b7acca2ff
jiangchaoyi <jiangchaoyi@bubugao.com>
2016-12-02 12:57:08 +0800

    N/A

--- source/js/conf/pagination.js
+++ source/js/conf/pagination.js
@@ -9,6 +9,8 @@ define(function(require, exports, module) {
     var $ = require('jquery');
     var Pagination = require('lib/ui/pagination/1.0.1/pagination');
 
+    var IO = require('lib/core/1.0.0/io/request');
+
     var pager = $('#jPagination');
 
     var random = function(min, max) {
@@ -19,20 +21,38 @@ define(function(require, exports, module) {
         pageSize: 10,
         totalCount: 100,
         displayInfo: true,
+        onPage:function(pageNum, e){
+            console.log('pageNum',pageNum);
+        }
 
         // Implements a async paging api method.
-        async: function(pageOpts) {
-            console.log('async paging options => ', pageOpts);
+        // async: function(pageOpts) {
+        //     console.log('async paging options => ', pageOpts);
 
-            var deferred = $.Deferred();
 
-            // simulating async paging
-            setTimeout(function() {
-                deferred.resolve({ totalCount: random(100, 500) });
-            }, 100);
+        //     var deferred = $.Deferred();
 
-            return deferred.promise();
-        }
+        //     IO.get($PAGE_DATA['baseStaticUrl'] + '/source/api/pager.json', function(data) {
+        //         console.log(data);
+        //         deferred.resolve(data)
+        //         // console.log(data);
+        //     });
+
+        //     return deferred.promise();
+
+        //     // // simulating async paging
+        //     // setTimeout(function() {
+        //     //     deferred.resolve({ totalCount: random(100, 500) });
+        //     // }, 100);
+        //     // 
+        //     // 
+
+        //     // io.get('',{},function(){
+
+        //     // });
+
+        //     // return deferred.promise();
+        // }
     });
 
     $('.selectPage').click(function() { pagination.selectPage(random(10, 100)); });
