b4c109804b2fdca3138be949995cb57c0ccdb848
jiangchaoyi <jiangchaoyi@bubugao.com>
2016-12-05 14:52:28 +0800

    N/A

--- source/js/conf/uploader.js
+++ source/js/conf/uploader.js
@@ -5,6 +5,7 @@ define(function(require, exports, module) {
     var Uploader = require('lib/plugins/uploader/1.0.1/uploader');
 
     var jChooseBg = $('#jChooseBg');
+    var jImgList = $('#jImgList');
 
     jChooseBg.on('click', function() {
         var uploader = new Uploader({
@@ -21,14 +22,17 @@ define(function(require, exports, module) {
             }, {
                 type: 'album',
                 options: {}
-            }]
+            }],
+            limit: 0, //上传限制，当是0的时候就代表无限制多选
+            selected: [] //选种的图片
         });
         uploader.on('ok', function(urls) {
             console.log(urls);
-            // for (var i = 0; i < urls.length; i++) {
-            //     var bg = nailBox.createBg(urls[i]);
-            //     nailBox.addBg(bg);
-            // }
+            var str = '';
+            for (var i = 0; i < urls.length; i++) {
+                str += '<img src="' + urls[i] + '">';
+            }
+            jImgList.html(urls);
             this.hide();
         });
         uploader.show();
