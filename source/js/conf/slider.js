e270421d6e0cf195292ec840026faff835b3bc36
jiangchaoyi <jiangchaoyi@bubugao.com>
2016-12-06 15:19:43 +0800

    N/A

---
+++ source/js/conf/slider.js
@@ -0,0 +1,36 @@
+define(function(require, exports, module) {
+    'use strict';
+
+    var $ = require('jquery');
+    require('lib/ui/slider/1.0.0/slider');
+    // var Slider = require('lib/ui/slider/3.0.4/slider');
+    // var swiper = require('lib/plugins/swiper/3.1.2/swiper');
+    // 
+
+    $('#jSlider').slide();
+
+    // var slider = new Slider('#jSlider', {
+    //     lazyLoad: {
+    //         loadingClass: 'img-error'
+    //     },
+    //     play: {
+    //         auto: false,
+    //         // interval: 4000,
+    //         // swap: true,
+    //         // pauseOnHover: true,
+    //         // restartDelay: 2500
+    //     },
+    //     // navigation: {
+    //     //     arrows: true,
+    //     //     toggleOnHover: false,
+    //     //     effect: 'slide',
+    //     //     nextArrow: '',
+    //     //     prevArrow: ''
+    //     // },
+    //     // callback: {
+    //     //     start: function(index) {},
+    //     //     loaded: function() {}
+    //     // }
+    // });
+
+});
